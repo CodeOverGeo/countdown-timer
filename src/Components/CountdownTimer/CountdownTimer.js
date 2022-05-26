832fba68491136c8484e6cce556d664f923354c4
Giovanni Rufino <giovannirufino@gmail.com>
2022-05-25 20:45:44 -0400

    initial commit with barebones app and styling

---
+++ src/Components/CountdownTimer/CountdownTimer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './CountdownTimer.css';
+
+const CoundownTimer = () => {
+  return (
+    <div className="countdown-timer">
+      <span>30</span>
+      <span>days</span>
+      <span>12</span>
+      <span>hours</span>
+      <span>04</span>
+      <span>minutes</span>
+      <span>32</span>
+      <span>seconds</span>
+    </div>
+  );
+};
+
+export default CoundownTimer;
